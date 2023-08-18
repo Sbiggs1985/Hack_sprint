44f64964aa87689b8b0df21a56ff271173e5d0f9
BradenE19 <6115@holbertonstudents.com>
2023-08-18 12:59:18 -0500

    commit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+const apiKey = "ad00caffb012448082150f6c32a9ae74"; // Replace with your actual API key
+const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}`;
+
+$.getJSON(url, function (data) {
+  $('DIV#recipes').text(data.name);
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   const searchInput = document.getElementById("searchInput");
   const searchButton = document.getElementById("searchButton");
@@ -10,20 +17,6 @@ searchButton.addEventListener("click", async () => {
     displayRecipes(recipes);
   }
 });
-
-async function fetchRecipes(query) {
-  const apiKey = "ad00caffb012448082150f6c32a9ae74"; // Replace with your actual API key
-  const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}`;
-
-  try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    return data.results;
-  } catch (error) {
-    console.error("Error fetching recipes:", error);
-    return [];
-  }
-}
 
 function displayRecipes(recipes) {
   resultsContainer.innerHTML = "";
