ccdd76e1b1e48e50e7f266bf0f12c64ac6e8868c
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-08-18 15:33:12 -0500

    update

--- scripts.js
+++ scripts.js
@@ -1,34 +1,66 @@
-const apiKey = "ad00caffb012448082150f6c32a9ae74"; // Replace with your actual API key
-const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}`;
+const settings = {
+	async: true,
+	crossDomain: true,
+	url: 'https://edamam-food-and-grocery-database.p.rapidapi.com/api/food-database/v2/parser?nutrition-type=cooking&category%5B0%5D=generic-foods&health%5B0%5D=alcohol-free',
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '340b6b0b2bmsh38d630a5cd772c9p15cebcjsnb3eb0ddfb0d0',
+		'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
+	}
+};
 
-$.getJSON(url, function (data) {
-  $('DIV#recipes').text(data.name);
+$.ajax(settings).done(function (response) {
+	console.log(response);
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const searchInput = document.getElementById("searchInput");
-  const searchButton = document.getElementById("searchButton");
-  const resultsContainer = document.getElementById("resultsContainer");
-
-searchButton.addEventListener("click", async () => {
-  const searchTerm = searchInput.value;
-  if (searchTerm) {
-    const recipes = await fetchRecipes(searchTerm);
-    displayRecipes(recipes);
-  }
+// scripts.js
+$('#searchButton').on('click', function () {
+    $.ajax(settings).done(function (response) {
+        displayResponse(response);
+    });
 });
 
-function displayRecipes(recipes) {
-  resultsContainer.innerHTML = "";
+function displayResponse(response) {
+    const responseContainer = $('#responseContainer');
+    responseContainer.empty();
 
-  recipes.forEach(recipe => {
-    const recipeCard = document.createElement("div");
-    recipeCard.classList.add("recipe-card");
-    recipeCard.innerHTML = `
-      <h2>${recipe.title}</h2>
-      <img src="${recipe.image}" alt="${recipe.title}">
-      <p>Ready in ${recipe.readyInMinutes} minutes</p>
-    `;
-    resultsContainer.appendChild(recipeCard);
-  });
+    const preElement = $('<pre>').text(JSON.stringify(response, null, 2));
+    responseContainer.append(preElement);
 }
+
+// Add this code after the previous code in scripts.js
+
+const searchButton = document.getElementById("searchButton");
+const searchInput = document.getElementById("searchInput");
+const resultsContainer = document.getElementById("resultsContainer");
+
+searchButton.addEventListener("click", () => {
+    const searchTerm = searchInput.value.trim();
+    if (searchTerm) {
+        searchFood(searchTerm);
+    }
+});
+
+function searchRecipesByMealType(mealType) {
+    const apiKey = '340b6b0b2bmsh38d630a5cd772c9p15cebcjsnb3eb0ddfb0d0'; // Replace with your API key
+    const apiUrl = `https://edamam-food-and-grocery-database.p.rapidapi.com/api/food-database/v2/parser?nutrition-type=cooking&ingr=${ingredient}&mealType=Breakfast`;
+
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => displayRecipes(data))
+            .catch(error => console.error("Error fetching data:", error));
+    }
+
+    function displayRecipes(recipes) {
+        resultsContainer.innerHTML = "";
+
+        recipes.forEach(recipe => {
+            const recipeCard = document.createElement("div");
+            recipeCard.classList.add("recipe-card");
+            recipeCard.innerHTML = `
+                <h2>${recipe.title}</h2>
+                <img src="${recipe.image}" alt="${recipe.title}">
+            `;
+            resultsContainer.appendChild(recipeCard);
+        });
+    };
